1addfd984ac6a29f951eec2de4739e63791b5f9d
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2025-08-29 00:11:33 +0100

    Implement user registration with phone number and UUID as primary key

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/signup', [UserController::class, 'signUp']);
 
 Route::get('/properties', [PropertyController::class, 'allProperties']);
 
